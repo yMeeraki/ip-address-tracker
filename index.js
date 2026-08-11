ca5f410d6b8084048e754105464a572f493c0704
Yukti Gupta <yuktigupta95@gmail.com>
2026-08-11 15:25:58 +0530

    Remove console log statement and fix semicolon in index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
 
-console.log(API_KEY);
-
 const baseURL = "https://geo.ipify.org/api/v2/country";
-const ipAddress = "8.8.8.8"
+const ipAddress = "8.8.8.8";
 
 fetch(`${baseURL}?apiKey=${API_KEY}&ipAddress=${ipAddress}`, { method: "GET" })
   .then((response) => response.json())
