ba627eaf51473c7d63a8602f06a91df8f7029450
Yukti Gupta <yuktigupta95@gmail.com>
2026-08-11 15:25:30 +0530

    Add fetch request to retrieve IP address data from geo.ipify API

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const API_KEY = import.meta.env.VITE_API_KEY;
 
 console.log(API_KEY);
 
+const baseURL = "https://geo.ipify.org/api/v2/country";
+const ipAddress = "8.8.8.8"
+
+fetch(`${baseURL}?apiKey=${API_KEY}&ipAddress=${ipAddress}`, { method: "GET" })
+  .then((response) => response.json())
+  .then((data) => console.log(data));
