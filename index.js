1546f59919e54e87ebe792724476c4ae98398ab4
Yukti Gupta <yuktigupta95@gmail.com>
2026-08-12 20:04:20 +0530

    Fix formatting and spacing in getLocation function for improved readability

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ document.getElementById("get-location").addEventListener("submit", getLocation);
 
 function getLocation(e) {
   e.preventDefault();
-  const API_KEY = import.meta.env.VITE_API_KEY;
 
+  const API_KEY = import.meta.env.VITE_API_KEY;
   const baseURL = "https://geo.ipify.org/api/v2/country,city";
   const ipAddress = document.getElementById("ip-address-input").value;
+
   fetch(`${baseURL}?apiKey=${API_KEY}&ipAddress=${ipAddress}`, {
     method: "GET",
   })
     .then((response) => response.json())
     .then((data) => showDetails(data));
+
+  document.getElementById("ip-address-input").value = " ";
 }
 
 function showDetails(data) {
@@ -35,4 +38,6 @@ function showDetails(data) {
   }).addTo(map);
 
   L.marker([data.location.lat, data.location.lng]).addTo(map);
+
+  //
 }
